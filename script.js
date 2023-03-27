8ab237147c9c8e8e94573c3d2135e7423037984c
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2023-03-27 18:03:44 +0300

    Turn to class

--- script.js
+++ script.js
@@ -1,67 +1,83 @@
-const myLibrary = [];
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
-// eslint-disable-next-line no-unused-vars
-function showForm() {
-    document.getElementById("book-form").style.display = "block";
-    document.querySelector("button").style.display = "none";
+class Library {
+  constructor() {
+    this.books = [];
   }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-}
+  addBook(book) {
+    this.books.push(book);
+  }
 
-function displayBooks() {
-  const bookList = document.getElementById("book-list");
+  removeBook(book) {
+    const index = this.books.indexOf(book);
+    if (index !== -1) {
+      this.books.splice(index, 1);
+    }
+  }
 
-  if (bookList == null) return;
+  displayBooks() {
+    const bookList = document.getElementById("book-list");
+
+    if (bookList == null) return;
 
     while (bookList.firstChild) {
       bookList.removeChild(bookList.firstChild);
     }
-     // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < myLibrary.length; i++) {
-    const book = myLibrary[i];
 
-    const li = document.createElement("li");
-    li.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${book.read ? "read" : "not read yet"}`;
-
-    bookList.appendChild(li);
+    for (const book of this.books) {
+      const li = document.createElement("li");
+      li.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${book.read ? "read" : "not read yet"}`;
+      bookList.appendChild(li);
+    }
   }
-  
-
- 
 }
 
+class Form {
+  constructor() {
+    this.form = document.getElementById("book-form");
+    this.titleInput = document.getElementById("title");
+    this.authorInput = document.getElementById("author");
+    this.pagesInput = document.getElementById("pages");
+    this.readInput = document.getElementById("read");
+    this.addButton = document.querySelector("button");
+    this.library = new Library();
 
+    this.form.addEventListener("submit", (event) => {
+      event.preventDefault();
 
-document.getElementById("book-form").addEventListener("submit", (event) => {
-  event.preventDefault();
+      const title = this.titleInput.value;
+      const author = this.authorInput.value;
+      const pages = this.pagesInput.value;
+      const read = this.readInput.checked;
 
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-  const read = document.getElementById("read").checked;
+      const book = new Book(title, author, pages, read);
+      this.library.addBook(book);
+      this.library.displayBooks();
 
-  const book = new Book(title, author, pages, read);
-  addBookToLibrary(book);
-  displayBooks();
+      this.form.style.display = "none";
 
-  document.getElementById("book-form").style.display = "none";
+      // clear the form
+      this.titleInput.value = "";
+      this.authorInput.value = "";
+      this.pagesInput.value = "";
+      this.readInput.checked = false;
 
-   // clear the form
-   document.getElementById("title").value = "";
-   document.getElementById("author").value = "";
-   document.getElementById("pages").value = "";
-   document.getElementById("read").checked = false;
+      this.addButton.style.display = "block";
+    });
+  }
 
-   document.querySelector("button").style.display = "block";
-   
-});
+  show() {
+    this.form.style.display = "block";
+    this.addButton.style.display = "none";
+  }
+}
 
+const form = new Form();
